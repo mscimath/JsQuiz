f76ab11267953b3c607b6390055d5b7637ce3b58
olgamadej <o1gmadej@gmail.com>
2023-02-25 15:41:10 +0100

    Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { useState} from 'react';
-import {questions} from './data/questions'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import {questions} from './data/questions';
 import JsQuiz from './components/JsQuiz';
 
-import './App.css';
 
 export default function App() {
-
+    const windowWidth = window.innerWidth;
+    const height = window.innerHeight;
 	return (
-		<>
-            <JsQuiz questions = {questions}/>
-        </>
+		<Router>
+            {/*<canvas style={{width : {windowWidth}, height: {height}}} id="background-canvas">
+            </canvas>*/}
+            <div className='circle'><br></br>width is {windowWidth}</div>
+            <div className="circle-one">height is{height}</div>
+            <div className="circle-two"></div>
+            <Link to="/js">js</Link>
+            <Routes>
+                <Route path='/js' element={<JsQuiz questions={questions} />}>js quiz</Route>
+            </Routes>
+        </Router>
 	);
 }
 
